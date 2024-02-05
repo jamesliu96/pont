794e48ddd3dcd1622f9fa89707a12801af7e8f25
James Liu <jamesliu96@users.noreply.github.com>
2024-02-05 19:28:23 +0800

    build

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders powered by text', () => {
-  render(<App />);
-  const textElement = screen.getByText(/powered by/i);
-  expect(textElement).toBeInTheDocument();
-});
