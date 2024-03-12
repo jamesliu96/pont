0f5b214aed95dd71ac5395c137f97dc71191255f
James Liu <jamesliu96@users.noreply.github.com>
2024-03-12 17:20:43 +0800

    build

--- cmd/pts/pts.go
+++ cmd/pts/pts.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	// function pont$$encrypt(suite: 1 | 2, key: string, plaintext: string, aad: string): Promise<string>
-	js.Global().Set("pont$$encrypt", ego.PromiseOf(func(this js.Value, args []js.Value) any {
+	js.Global().Set("pont$$encrypt", ego.AsyncFuncOf(func(this js.Value, args []js.Value) any {
 		suite, key, plaintext, aad := args[0].String(), args[1].String(), args[2].String(), args[3].String()
 		ciphertext, err := pont.Encrypt(pont.Suite(suite), key, plaintext, aad)
 		if err != nil {
@@ -19,7 +19,7 @@ func main() {
 	}))
 
 	// function pont$$decrypt(key: string, ciphertext: string): Promise<{ suite: 0 | 1; plaintext: string; aad: string }>
-	js.Global().Set("pont$$decrypt", ego.PromiseOf(func(this js.Value, args []js.Value) any {
+	js.Global().Set("pont$$decrypt", ego.AsyncFuncOf(func(this js.Value, args []js.Value) any {
 		key, ciphertext := args[0].String(), args[1].String()
 		suite, plaintext, aad, err := pont.Decrypt(key, ciphertext)
 		if err != nil {
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/jamesliu96/pont
 go 1.22.0
 
 require (
-	github.com/jamesliu96/ego v1.1.0
+	github.com/jamesliu96/ego v1.3.0
 	golang.org/x/crypto v0.21.0
 )
 
